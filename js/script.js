b0897a1946955f4694aaf3d0c77c092aa5232c5f
ohyama-webtest <66063350+ohyama-webtest@users.noreply.github.com>
2022-09-23 13:48:48 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
-$(function(){
-  $('.question').click(function(){
+$(function () {
+  $('.question').click(function () {
     $(this).next().slideToggle();
     $(this).find('p').toggleClass('on');
+  });
+
+  $(".openbtn1").click(function () {
+    $(this).toggleClass('active');
+    $('.header-sp').toggleClass('active');
+  });
+  $('.header-sp, .header-sp li a' ).click(function() {
+    $('.openbtn1').toggleClass('active');
+    $('.header-sp').toggleClass('active');
+  });
+
+  $('a[href^="#"]').click(function () {
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    let position = target.offset().top;
+    let speed = 500;
+    $("html, body").animate({
+      scrollTop: position
+    }, speed, "swing");
+    return false;
   });
 });
